11a8b7267e7e2c6a63db1bbd4197d8f0733f06a0
Harshita choudhary <harshitachoudhary130@gmail.com>
2024-12-28 16:05:04 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail ="harshita@gmail.com"
+accountcity="jaipur"
+var accountPassword ="12345"
+let accountState
+// accountId=2 not allowed 
+accountcity="Dehli"
+accountEmail="hc@gmail.com"
+accountPassword="222222"
+//console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountState,accountcity]);
+/* perfer not to use var because of issue in block scope and functional block*/ 
