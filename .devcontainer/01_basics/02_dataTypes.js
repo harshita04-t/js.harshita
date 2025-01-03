bcce488864870796ac7d7cb08e8ceac0ab248583
Harshita choudhary <harshitachoudhary130@gmail.com>
2025-01-03 07:57:28 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3) we are using nodejs,not browser
+
+console.log(3+3)//code readability should be high
+
+console.log("Harshita")
+
+let name = "Harshita"
+//string dataTypes
+let age = 20 
+//number dataTypes => 2 to power 53
+let isloggedIn = false
+// boolean dataTypes
+// bigint use for large number
+// null => standlone value it is a representation of empty value, it is a object
+// undefined => it is not defined yet
+// symbol => it is us in react ,unique
+ 
+console.log(typeof null);
