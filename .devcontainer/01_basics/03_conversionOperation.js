1b53a8a1a1efca7e2ac91df63e4067450dfc2995
Harshita choudhary <harshitachoudhary130@gmail.com>
2025-01-03 08:31:47 +0000

    conversionofdataType

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+//let score = "33abc"
+//let score =   true //undefined,null 
+//console.log(typeof score);
+//let valueInNumber =  Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//"33" => 33 when we convert a number string it can convert easily 
+//"33abc" => NaN it can't be covert this will give value NaN
+// true => 1 , false => 0
+let isloggedIn = 1
+let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+// 1 => true; 0=> false
+//" "=> false
+//"Harshita" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
