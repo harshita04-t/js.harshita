792307ad539ffec865aae4f6d63135e408263682
Harshita choudhary <harshitachoudhary130@gmail.com>
2025-01-03 08:56:01 +0000

    opertions

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -15,5 +15,31 @@ let booleanIsLoggedIn = Boolean(isloggedIn)
 //"Harshita" => true
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+//********************** Operations *******************/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "hello"
+let str2 = " harshita"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 +  2 + "2");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
