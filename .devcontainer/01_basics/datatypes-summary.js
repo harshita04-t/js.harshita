48391a93c90506504c53d8ed45e638c6f4bd1552
Harshita choudhary <harshitachoudhary130@gmail.com>
2025-01-04 12:28:56 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,13 @@
+// Primitive
+// 7 Types : String, Number, Boolean, null(Empty), undefined, Symbol(it is use to make variable unique),BigInt
+
+// Reference Type(NoN-primitive)
+//Array, Objects, functions
+const score = 100 
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log( Id === anotherId);
