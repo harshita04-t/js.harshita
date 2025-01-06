a32ad7367d0282caadbc8864e48049c06a849147
Harshita choudhary <harshitachoudhary130@gmail.com>
2025-01-06 10:36:31 +0000

    string

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,43 @@
+const name = "harshita_"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('harshita_hc_com')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-9,4)
+console.log(anotherString);
+
+const newStringOne = "  harshta"
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://harshita.com/harshita%20choudhary"
+console.log(url.replace('%20','*'));
+
+console.log(url.includes('harshita'));
+console.log(url.includes('vanshika'));
+console.log(gameName.split('_'));
+
+const name1 = "Hello World, "
+console.log(name1.anchor('Hello'));
+console.log(name1.at('4'));
+console.log(name1.big('4'));
+console.log(name1.blink('4'));
+
+console.log(name1.charCodeAt(2));
+console.log(name1.codePointAt(2));
+
+console.log(name1.concat('my name is ', 'harshita choudhary'));
