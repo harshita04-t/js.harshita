35bfea632ad7e09f421d3f641f935b55d4ac8510
Harshita choudhary <harshitachoudhary130@gmail.com>
2025-01-10 16:21:26 +0000

    arrays

--- .devcontainer/02_basics/01_arrays.js
+++ .devcontainer/02_basics/01_arrays.js
@@ -1,1 +1,36 @@
-  
+  //arrays
+  const myArr =[0,1,2,3,4]
+  const myHeors = ["Rm","jin","J-hope"]
+
+  const myArr2 = new Array(1,2,3,4)
+  console.log(myArr[0]);
+  //Array Mrthods
+//   myArr.push(6)//add the values in array
+//   myArr.push(7)
+//   console.log(myArr);
+//   myArr.pop()
+// myArr.unshift(6)
+//    myArr.shift()
+
+//   console.log(myArr.includes(8)) // give boolean result 
+//   console.log(myArr.indexOf(8))
+
+//  const newArr = myArr.join()
+
+//   console.log(myArr);
+//   console.log(typeof myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+ 
+//slice,splice
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log("C",myArr);
+console.log(myn2);
