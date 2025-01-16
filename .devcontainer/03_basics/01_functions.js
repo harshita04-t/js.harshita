bceb7087f5780dc7581148d7fe17c71348e2a7dc
Harshita choudhary <harshitachoudhary130@gmail.com>
2025-01-16 15:29:09 +0000

    functions done

---
+++ .devcontainer/03_basics/01_functions.js
@@ -0,0 +1,51 @@
+function sayMyName(){
+    
+console.log("H");
+console.log("A");
+console.log("R");
+console.log("S");
+console.log("H");
+console.log("I");
+console.log("T");
+console.log("A");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+// addTwoNumbers(3,4)
+
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+// console.log(addTwoNumbers(3,4));
+
+// const result = addTwoNumbers(3, 4)
+// console.log("Result:", result);
+
+
+function loginUserMessage(username = "sam"){ // we are given default  value in parameters
+    if(username === undefined){ // if (!username)
+       console.log("Please enter a username");
+       
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Harshita"));
+//console.log(loginUserMessage()); // if we don't give any argument in function the it well give undefined
+console.log(loginUserMessage("harshita")); // but if arguments are given then will over write the default value which is give as a parameter in function
+
+                                             
+
+
+
+
+
